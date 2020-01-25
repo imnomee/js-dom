1c19f478bc6e8970401538d1679846c07ec18c61
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-01-25 09:59:35 +0000

    code and samples updated

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const myList = document.getElementsByTagName('li');
 //Selecing using Classes
 const notPurple = document.getElementsByClassName('not-purple');
 
+//Selecting using QuerySelectors
+//const notPurple = document.querySelectorAll('.not-purple');
+
+const evens = document.querySelectorAll('li:nth-child(even)');
+
 //Selecting using ID
 //const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
@@ -29,6 +34,20 @@ for (let i = 0; i < myList.length; i++) {
 
 //Change color of not found list using for loop
 for (let i = 0; i < notPurple.length; i++) {
-    notPurple[i].style.color = 'red';
+    notPurple[i].style.color = 'ivory';
 
+}
+
+//Change color of evens in list using for loop
+for (let i = 0; i < evens.length; i++) {
+    evens[i].style.backgroundColor = 'grey';
+
+}
+
+//Adding colors from colors array using iD collection
+var listItems=document.getElementById('rainbow');
+var colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
+
+for(var i = 0; i < colors.length; i ++) {
+  listItems.children[i].style.color = colors[i];    
 }
